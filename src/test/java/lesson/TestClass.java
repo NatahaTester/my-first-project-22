5279f36d1dff5e3d19255513fab82a80074f0490
Natalija <milashk222@gmail.com>
2022-11-25 14:18:06 +0200

    пытаюсь выложить чтото в гит

--- src/test/java/lesson/TestClass.java
+++ src/test/java/lesson/TestClass.java
@@ -3,7 +3,7 @@ package lesson;
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("тест как выкладывать домашку");
+        System.out.println("тест как выкладывать домашку???");
     }
 
 }
