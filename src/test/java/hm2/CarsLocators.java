745fe11248a218350a14c3a82ab12f2988190ef3
Natalija <milashk222@gmail.com>
2022-11-25 17:08:27 +0200

    locators home work

--- src/test/java/hm2/CarsLocators.java
+++ src/test/java/hm2/CarsLocators.java
@@ -1,6 +1,20 @@
 package hm2;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CarsLocators {
+    private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+    @Test
+    public void openCarsPage() {
+        System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.manage().window().maximize();
+        browser.get("https://www.discovercars.com/");
 
+        browser.findElement(ACCEPT_COOKIES_BTN).click();
 
+    }
 }
